0782f150a92e22ac519f9e222f56a9d4a4387e7a
JHON-DESKTOP\jhonl <jhon.lopera05rep@gmail.com>
2023-03-12 17:58:55 -0500

    test5

--- analytics/modules/apirest/apirest_endpoints.py
+++ analytics/modules/apirest/apirest_endpoints.py
@@ -12,7 +12,7 @@ class AnalyticEndPoints:
 
     @classmethod
     async def _index_endpoint(cls):
-        message = "Analitycs APIREST is working...Test4"
+        message = "Analitycs APIREST is working...Test5"
         Logger.log(message)
         return json_message(message)
 
