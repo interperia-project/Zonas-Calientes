294eb9fa5adc0d53db87cfedbd4b69fa153f2efe
jhon.lopera <anderson.lopera@sourcemeridian.com>
2023-03-12 18:38:16 -0500

    test6

--- analytics/modules/apirest/apirest_endpoints.py
+++ analytics/modules/apirest/apirest_endpoints.py
@@ -12,7 +12,7 @@ class AnalyticEndPoints:
 
     @classmethod
     async def _index_endpoint(cls):
-        message = "Analitycs APIREST is working...Test5"
+        message = "Analitycs APIREST is working...Test6"
         Logger.log(message)
         return json_message(message)
 
